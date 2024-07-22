c46f90999e610f1919f57939498eeb973a9f9f73
Vishwa Joshi <107062706+Vishwa-J@users.noreply.github.com>
2024-07-22 18:54:12 +0530

    Create validPali2.cpp

---
+++ String/validPali2.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/valid-palindrome-ii/description/
+class Solution {
+public:
+    bool isPalindrome(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        if(isPalindrome(s,left,right)==true){
+            return true;
+        }
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};
